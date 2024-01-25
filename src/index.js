a91da3c19b85c1ee0cc109736255a3daa3fd011d
Brendan Dempsey <bjd1412@gmail.com>
2024-01-25 02:36:16 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  form = document.querySelector('form')
+  form.addEventListener("submit", (e) => { 
+  e.preventDefault()
+  handleSubmit(e.target.newdescription.value)
+  
+  })
+})
+
+function handleSubmit (ntd) {
+let p = document.createElement('p')
+let btn = document.createElement('button')
+btn.textContent = 'complete'
+btn.addEventListener('click', handleDeleter)
+p.textContent = `${ntd} `
+p.appendChild(btn)
+document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDeleter (e) {
+e.target.parentNode.remove()
+}
